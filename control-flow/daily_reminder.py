fda309ce965e36ba9f41668552cf48461f60fe08
Agent1c <medicebyb@gmail.com>
2024-09-22 08:42:26 +0200

    4. Personal Daily Reminder

---
+++ control-flow/daily_reminder.py
@@ -0,0 +1,17 @@
+print("Personal Daily Reminder") #app header/tittle
+
+#prompting user inputs for 1. Task, 2 Priority and 3. Time bond
+
+task = input("Enter your task: ")
+priority = input("Priority (high/medium/low): ")
+time_bound = input("Is it time-bound? (yes/no): ")
+
+#evaluating cases based on users inputs
+match priority:
+    case "high" | "medium" | "low":
+        if time_bound == "yes":
+            print(f"Reminder: {task} is a high priority task that requires immediate attention today!")
+        if time_bound == "no":
+            print(f"Note: {task} is a low priority task. Consider completing it when you have free time.")
+    case _ :
+        print("I dont have a reminder or time bond for your request")
