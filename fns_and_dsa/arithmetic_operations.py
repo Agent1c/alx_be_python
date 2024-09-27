c49fa74cbdf8a557a97854e81bbc2c5379948038
Agent1c <medicebyb@gmail.com>
2024-09-27 21:16:28 +0200

    arithmetic_operations.py function

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -30,6 +30,11 @@ def perform_operation(num1, num2, operation):
         return num1 * num2
 
     def divide(num1, num2):
-        return num1 / num2
+        if num2 ==0:
+            print("Cannot Divide by Zero.")
+        elif num1 ==0:
+            print("Cannot Divide by Zero.")
+        else:
+            return num1 / num2
         
         perform_operation(num1, num2, operation)
