8b2ccc376b5ecb2071e30cefb94561d26c5534f1
Agent1c <medicebyb@gmail.com>
2024-09-29 21:20:40 +0200

    temp_conversion_tool.py

--- fns_and_dsa/temp_conversion_tool.py
+++ fns_and_dsa/temp_conversion_tool.py
@@ -1,6 +1,6 @@
 # Define global conversion factors
-FAHRENHEIT_TO_CELSIUS_FACTOR = 5 / 9
 CELSIUS_TO_FAHRENHEIT_FACTOR = 9 / 5
+FAHRENHEIT_TO_CELSIUS_FACTOR = 5 / 9
 
 # Define the conversion functions
 def convert_to_celsius(fahrenheit):
@@ -15,13 +15,15 @@ def main():
         temperature = float(input("Enter the temperature to convert: "))
         unit = input("Is this temperature in Celsius or Fahrenheit? (C/F): ").capitalize()
 
-        if unit.capitalize() == "F":
-            print(f"{temperature}°F is equal to {convert_to_celsius(temperature):.2f}°C")
-        elif unit.capitalize() == "C":
-            print(f"{temperature}°C is equal to {convert_to_fahrenheit(temperature):.2f}°F")
-        else:
-            print(("Invalid temperature unit. Please enter F or C."))
-            break        
+        match unit:
+            case _ if unit.capitalize() == "F":
+              print(f"{temperature}°F is equal to {convert_to_celsius(temperature):.2f}°C")
+            case _ if unit.capitalize() == "C":
+                print(f"{temperature}°C is equal to {convert_to_fahrenheit(temperature):.2f}°F")
+            case _ :
+                print(("Invalid temperature unit. Please enter F or C."))
+                break
+    
 
 if __name__ == "__main__":
     main()
