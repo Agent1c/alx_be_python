38b6eb22c149e2c22281d1e6f3b52913bf96b160
Agent1c <medicebyb@gmail.com>
2024-10-03 22:42:10 +0200

    0. Create a Simple Bank Account Class

---
+++ programming_paradigm/bank_account.py
@@ -0,0 +1,35 @@
+class BankAccount:
+    def __init__(self, account_balance):
+        self.account_balance = account_balance
+
+    #Depositing Method
+    def deposit(self,amount):
+        while True:
+            """Looping thru the deposit function"""
+            try:
+
+                amount = float(input("Deposit: "))
+                self.account_balance += amount
+                print(f"Your Current balance is ${amount}")
+                # pass
+            except ValueError as e:
+                print(f"Invalid input {e}")  #Raising value error
+    #Withdrawal Method
+    def withdraw(self,amount):
+        while True:
+            """Looping thru the withdrawal function"""
+            try:
+                amount = float(input("Withdraw amount: "))
+                if self.account_balance >= amount:
+                    self.account_balance -= amount
+                    print(f"You withdrew ${amount}")
+                else:
+                    print(f"Insufficient Funds {self.account_balance}")
+            except ValueError as e:
+                print(f"Invalid input, try again {e}") #Raising value error
+
+    # Displaying current balance.
+    def display_balance(self):
+        print(f"Your current balance is ${self.account_balance}")
+        
+
