4afe914d5eadfee856c2ac6b02094205197cc474
Agent1c <medicebyb@gmail.com>
2024-10-05 20:30:33 +0200

    2. Writing Unit Tests for a Simple Calculator Class

--- programming_paradigm/test_simple_calculator.py
+++ programming_paradigm/test_simple_calculator.py
@@ -15,24 +15,24 @@ class TestSimpleCalculator(unittest.TestCase):
         self.assertEqual(self.calc.add(10, 11), 21)
         self.assertEqual(self.calc.add(-18, 90), 72)
 
-    def test_subtract(self):
-        """Test the subtract method."""
+    def test_subtraction(self):
+        """Test the subtraction method."""
         self.assertEqual(self.calc.subtract(8, 3), 5)
         self.assertEqual(self.calc.subtract(12, 6), 6)
         self.assertEqual(self.calc.subtract(-1, 6), -7)
         self.assertEqual(self.calc.subtract(15, 11), 4)
         self.assertEqual(self.calc.subtract(-18, 90), -108)        
 
-    def test_multiply(self):
-        """Test the addition method."""
+    def test_multiplication(self):
+        """Test the multiplication method."""
         self.assertEqual(self.calc.multiply(28, 6), 168)
         self.assertEqual(self.calc.multiply(-8, 3), -24)
         self.assertEqual(self.calc.multiply(18, 5), 90)
         self.assertEqual(self.calc.multiply(10, 11), 110)
         self.assertEqual(self.calc.multiply(-18, 90), -1620)
        
-    def test_divide(self):
-        """Test the addition method."""
+    def test_division(self):
+        """Test the division method."""
         self.assertEqual(self.calc.divide(0, 5), 0)
         self.assertEqual(self.calc.divide(15, 3), 5)
         self.assertEqual(self.calc.divide(30, 10), 3)
